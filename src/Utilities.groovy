1319f8d9afcbac68e33748551bff4f5bb903b52e
bayamake <ken.bayama@ebiznext.com>
2019-12-04 11:05:37 +0100

    formatted version of Library.groovy

--- src/Utilities.groovy
+++ src/Utilities.groovy
@@ -1,36 +1,38 @@
-package org.shared
 class Utilities implements Serializable {
 
-    def sbtHome = tool 'sbt-0.13.11'
 
     def steps
-    Utilities(steps) {this.steps = steps}
+
+    Utilities(steps) {
+        this.steps = steps
+    }
+
 
     def checkOutFrom(sourceBranch, credential, url) {
         steps.git branch: "${sourceBranch}", credentialsId: "${credential}", url: "${url}"
     }
 
     def compileService() {
-        try{
-            steps.sh "java -XX:+CMSClassUnloadingEnabled -Xms1024m -Xmx4096m -Xss4m -XX:PermSize=1024m -Dsbt.log.noformat=true -jar ${sbtHome}/bin/sbt-launch.jar clean pack"
+        try {
+            steps.sh "java -XX:+CMSClassUnloadingEnabled -Xms1024m -Xmx4096m -Xss4m -XX:PermSize=1024m -Dsbt.log.noformat=true -jar ${steps.tool 'sbt-0.13.11'}/bin/sbt-launch.jar clean pack"
         }
-        catch(error){
+        catch (error) {
             throw error
         }
     }
 
-    def archiving = steps.archiveArtifacts artifacts: 'target/pack/**'
+    //def archiving = steps.archiveArtifacts artifacts: 'target/pack/**'
 
-    def deploy (workspace,service,catchup_start_date,catchup_end_date,DeployOn,platform) {
-        try{
+    def deploy(workspace, service, catchup_start_date, catchup_end_date, DeployOn, platform) {
+
+        try {
             steps.sh "ssh -t -T devops@localhost 'cd ${workspace}/ansible && /var/lib/jenkins/env/bin/ansible-playbook deploy.yml --tags all -e \"service=${service}\" -e \"catchup_start_date=${catchup_start_date}\" -e \"catchup_end_date=${catchup_end_date}\" -e \"platforms_env=${DeployOn}\" -i platforms/${platform}/inventory.ini'"
         }
-        catch(error){
+        catch (error) {
             throw error
         }
+
     }
-
-
 
 
 }
